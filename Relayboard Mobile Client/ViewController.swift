60035985416dc629ee76ed13f76cef1adf4f482b
user <user@macbook.local>
2018-01-13 21:06:40 +0300

    Initial Commit

---
+++ Relayboard Mobile Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Relayboard Mobile Client
+//
+//  Created by user on 13.01.2018.
+//  Copyright © 2018 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
